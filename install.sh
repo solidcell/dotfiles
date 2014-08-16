6b879f0baa4a6f4334e3f13a68db72d1056a1865
Jesse Farless <solidcell@gmail.com>
2014-08-15 17:54:11 -0700

    Add red, green, and yellow colors to status messages

--- install.sh
+++ install.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
+ESC_SEQ='\x1B'
+RESET="${ESC_SEQ}[0m"
+GREEN="${ESC_SEQ}[0;32m"
+YELLOW="${ESC_SEQ}[0;33m"
+RED="${ESC_SEQ}[0;31m"
+
 link_tmux_dotfile () {
+  echo '[tmux] linking .tmux.conf'
   if ! [ -e $HOME/.tmux.conf ]
   then
     ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-    echo '       linked'
+    echo "       ${GREEN}linked!${RESET}"
   else
-    echo '       already exists. skipping...'
+    echo "       ${YELLOW}already exists. skipping...${RESET}"
   fi
 }
 
 install_reattach_to_user_namespace () {
+  echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
   if [ `command -v brew | wc -l` -eq 1 ]
   then
     if [ `brew list | grep 'reattach-to-user-namespace' | wc -l` -eq 0 ]
     then
       brew install reattach-to-user-namespace
-      echo '       installed'
+      echo "       ${GREEN}installed!${RESET}"
     else
-      echo '       already installed. skipping...'
+      echo "       ${YELLOW}already installed. skipping...${RESET}"
     fi
   else
-    echo '[tmux] brew is not installed.'
-    echo '       install brew and re-run this script.'
+    echo "       ${RED}brew is not installed. install brew and re-run this script.${RESET}"
   fi
 }
 
@@ -30,13 +37,10 @@ prepare_tmux () {
   echo '\n[tmux] started'
   if [ `command -v tmux | wc -l` -eq 1 ]
   then
-    echo '[tmux] linking .tmux.conf'
     link_tmux_dotfile
-    echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
     install_reattach_to_user_namespace
   else
-    echo '[tmux] tmux is not installed.'
-    echo '       install tmux and re-run this script.'
+    echo "       ${RED}tmux is not installed. install tmux and re-run this script.${RESET}"
   fi
   echo '[tmux] finished'
 }
