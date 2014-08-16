ebe84888d8a0f8f146d880499dd4817ec41ea391
Jesse Farless <solidcell@gmail.com>
2014-08-15 17:29:34 -0700

    Organize installation script into functions

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-echo '******* Installation started *******'
-
-##### Install tmux ######
-echo '\n[tmux] started'
-if [ `command -v tmux | wc -l` -eq 1 ]
-then
-  echo '[tmux] linking .tmux.conf'
+link_tmux_dotfile () {
   if ! [ -e $HOME/.tmux.conf ]
   then
     ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
@@ -14,7 +8,9 @@ then
   else
     echo '       already exists. skipping...'
   fi
-  echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
+}
+
+install_reattach_to_user_namespace () {
   if [ `command -v brew | wc -l` -eq 1 ]
   then
     if [ `brew list | grep 'reattach-to-user-namespace' | wc -l` -eq 0 ]
@@ -28,10 +24,23 @@ then
     echo '[tmux] brew is not installed.'
     echo '       install brew and re-run this script.'
   fi
-else
-  echo '[tmux] tmux is not installed.'
-  echo '       install tmux and re-run this script.'
-fi
-echo '[tmux] finished'
+}
 
+prepare_tmux () {
+  echo '\n[tmux] started'
+  if [ `command -v tmux | wc -l` -eq 1 ]
+  then
+    echo '[tmux] linking .tmux.conf'
+    link_tmux_dotfile
+    echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
+    install_reattach_to_user_namespace
+  else
+    echo '[tmux] tmux is not installed.'
+    echo '       install tmux and re-run this script.'
+  fi
+  echo '[tmux] finished'
+}
+
+echo '******* Installation started *******'
+prepare_tmux
 echo '\n******* Installation complete *******'
