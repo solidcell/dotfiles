b0fa131359465b0a9eea063151250c83395c3df4
Jesse Farless <solidcell@gmail.com>
2014-08-15 17:17:11 -0700

    Add check for tmux command existence

--- install.sh
+++ install.sh
@@ -2,21 +2,27 @@ echo '******* Installation started *******'
 
 ##### Install tmux ######
 echo '\n[tmux] started'
-echo '[tmux] linking .tmux.conf'
-if ! [ -e $HOME/.tmux.conf ]
+if [ `command -v tmux | wc -l` -eq 1 ]
+then
+  echo '[tmux] linking .tmux.conf'
+  if ! [ -e $HOME/.tmux.conf ]
   then
     ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
     echo '       linked'
   else
     echo '       already exists. skipping...'
-fi
-echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
+  fi
+  echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
   if [ `brew list | grep 'reattach-to-user-namespace' | wc -l` -eq 0 ]
   then
     brew install reattach-to-user-namespace
     echo '       installed'
   else
     echo '       already installed. skipping...'
+  fi
+else
+  echo '[tmux] tmux is not installed.'
+  echo '       install tmux and re-run this script.'
 fi
 echo '[tmux] finished'
 
