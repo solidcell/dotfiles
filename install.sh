7a6c6a04527cf1c76a1c0ae03304938716e76c66
Jesse Farless <solidcell@gmail.com>
2014-08-19 13:54:45 -0700

    Add #print_message and DRY the prog name and spacing

--- install.sh
+++ install.sh
@@ -6,8 +6,20 @@ GREEN="${ESC_SEQ}[0;32m"
 YELLOW="${ESC_SEQ}[0;33m"
 RED="${ESC_SEQ}[0;31m"
 
+print_message () {
+  tag="[${CURRENT_PROG}]"
+  let length=${#tag}+1
+  if [ "$2" != "" ]
+  then
+    echo "${tag} ${1}"
+  else
+    ch=' '
+    printf '%*s' "$length" | tr ' ' "$ch"
+    echo $1
+  fi
+}
+
 dotfile_exists () {
-  echo "[FIXME] linking .$1"
   test -e $HOME/.$1
 }
 
@@ -21,82 +33,86 @@ command_exists () {
 }
 
 link_tmux_dotfile () {
-  echo '[tmux] linking .tmux.conf'
+  print_message 'linking .tmux.conf' true
   if dotfile_exists tmux.conf
   then
-    echo "       ${GREEN}already exists. skipping...${RESET}"
+    print_message "${GREEN}already exists. skipping...${RESET}"
   else
     ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-    echo "       ${GREEN}linked!${RESET}"
+    print_message "GREEN}linked!${RESET}"
   fi
 }
 
 link_gitconfig_dotfile () {
-  echo '[git] linking .gitconfig'
+  print_message 'linking .gitconfig' true
   if dotfile_exists gitconfig
   then
-    echo "       ${GREEN}already exists. skipping...${RESET}"
+    print_message "${GREEN}already exists. skipping...${RESET}"
   else
     ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
-    echo "       ${GREEN}linked!${RESET}"
+    print_message "${GREEN}linked!${RESET}"
   fi
 }
 
 install_with_brew () {
   if command_exists $1
   then
-    echo "       ${GREEN}already installed. skipping...${RESET}"
+    print_message "${GREEN}already installed. skipping...${RESET}"
     return 0
   else
     if command_exists brew
     then
-      brew install $1 && echo "       ${GREEN}installed!${RESET}"
+      brew install $1 && print_message "${GREEN}installed!${RESET}"
       return $?
     else
-      echo "       ${RED}brew is not installed. install brew and re-run this script.${RESET}"
+      print_message "${RED}brew is not installed. install brew and re-run this script.${RESET}"
       return 0
     fi
   fi
 }
 
 install_reattach_to_user_namespace () {
-  echo '[tmux] installing reattach-to-user-namespace (for copy/paste support)'
+  print_message 'installing reattach-to-user-namespace (for copy/paste support)' true
   install_with_brew reattach-to-user-namespace
 }
 
 install_git () {
-  echo '[git] checking for git'
+  print_message 'checking for git' true
   if command_exists git
   then
-    echo "      ${GREEN}already installed.${RESET}"
+    print_message "${GREEN}already installed.${RESET}"
     return 0
   else
-    echo "      ${RED}git is not installed. install git and re-run this script.${RESET}"
+    print_message "${RED}git is not installed. install git and re-run this script.${RESET}"
     return 1
   fi
 }
 
 install_tmux () {
-  echo '[tmux] installing tmux'
+  print_message 'installing tmux' true
   install_with_brew tmux
   return $?
 }
 
 prepare_tmux () {
-  echo '\n[tmux] started'
+  CURRENT_PROG=tmux
+  echo ''
+  print_message 'started' true
   if install_tmux; then
     link_tmux_dotfile
     install_reattach_to_user_namespace
   fi
-  echo '[tmux] finished'
+  print_message 'finished' true
 }
 
 prepare_git () {
-  echo '\n[git] started'
+  CURRENT_PROG=git
+  echo ''
+  print_message 'started' true
   if install_git; then
     link_gitconfig_dotfile
   fi
-  echo '[git] finished'
+  print_message 'finished' true
 }
 
 echo '******* Installation started *******'
