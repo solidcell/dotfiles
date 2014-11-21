3e12002ca4e259cf5ac94e86c27323689a044d13
Jesse Farless <solidcell@gmail.com>
2014-11-20 20:38:04 -0800

    Remove "Radio" from station names and improve colors

--- config/pianobar/event_command.rb
+++ config/pianobar/event_command.rb
@@ -7,6 +7,8 @@ if event == 'songstart'
   STDIN.each_line { |line| d.store(*line.chomp.split('=', 2)) }
 
   File.open("#{ENV['HOME']}/.config/pianobar/current-song.txt", 'w') do |f|
-    f.write("#[fg=colour0]#{d['title']} - #{d['artist']} [#{d['stationName']}]#[fg=default,bg=default] ")
+    station_name = d['stationName'].chomp(' Radio')
+    reset = '#[fg=default,bg=default]'
+    f.write("#[fg=colour0]#{d['title']} #[fg=colour22]by#{reset} #{d['artist']} #[fg=colour22][#{station_name}]#{reset} ")
   end
 end
